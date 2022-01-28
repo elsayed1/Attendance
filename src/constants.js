864fa1829d74d2c38170fab07a2c1e00353eb0ac
elsayed1 <elsayed0alaa@gmail.com>
2022-01-28 15:34:41 +0200

    feat:add app constants

---
+++ src/constants.js
@@ -0,0 +1,8 @@
+export const MINUTES_IN_HOUR = 60;
+export const DAILY_WORKING_HOURS = 8;
+export const DAYS_IN_WEEK = 7;
+
+export const navBarPages = [
+  { name: 'Home', route: '' },
+  { name: 'Work Hours', route: 'working-hours' }
+];
